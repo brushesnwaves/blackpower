0b6b220a2506d4bc46e042c6a3104c8f20b226da
dartanyon <djprat14@icloud.com>
2019-07-20 11:53:20 -0700

    fix-1error

--- week1/temp.py
+++ week1/temp.py
@@ -1,3 +1,9 @@
+
+OUT_OF_BOUNDS =float('-inf')
+# print(OUT_OF_BOUNDS)
+
+
+
 def calculate(first_number, second_number, operator):
     result = 0
     if operator == "+":
@@ -9,7 +15,7 @@ def calculate(first_number, second_number, operator):
     elif operator  == "/":
         result = (first_number / second_number)
     else:
-        result =-1
+        result =OUT_OF_BOUNDS
     return result
 # print  (calculate(1,2,"+"))
 
@@ -17,14 +23,14 @@ first_number = input("what is the first number to add?")
 first_number = int(first_number)
 
 second_number = input("what is the second number to add?")
-second_number =int(second_number)
+second_number = int(second_number)
 print(first_number, second_number)
 print(first_number +second_number)
 operator = input ("what operation do you want me to do(+, -, *, /)?")
 
 result = calculate(first_number , second_number, operator)
 
-if result == -1:
+if result ==OUT_OF_BOUNDS:
     print("you need a valid operator(+, -, *, /)")
 
 else:
