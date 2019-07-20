e0e3e8dd0b66ca0ddb713b7d04be4d1eaf155c3e
dartanyon <djprat14@icloud.com>
2019-07-20 12:27:17 -0700

    sup

--- week1/temp.py
+++ week1/temp.py
@@ -18,20 +18,26 @@ def calculate(first_number, second_number, operator):
         result =OUT_OF_BOUNDS
     return result
 # print  (calculate(1,2,"+"))
+more_input = True
+while more_input:
+    first_number = input("what is the first number to add?")
+    first_number = int(first_number)
 
-first_number = input("what is the first number to add?")
-first_number = int(first_number)
+    second_number = input("what is the second number to add?")
+    second_number = int(second_number)
+    print(first_number, second_number)
+    print(first_number +second_number)
+    operator = input ("what operation do you want me to do(+, -, *, /)?")
 
-second_number = input("what is the second number to add?")
-second_number = int(second_number)
-print(first_number, second_number)
-print(first_number +second_number)
-operator = input ("what operation do you want me to do(+, -, *, /)?")
+    result = calculate(first_number , second_number, operator)
 
-result = calculate(first_number , second_number, operator)
+    if result ==OUT_OF_BOUNDS:
+        print("you need a valid operator(+, -, *, /)")
 
-if result ==OUT_OF_BOUNDS:
-    print("you need a valid operator(+, -, *, /)")
+    else:
+        print(result)
+    if input("Another calculation (y/n)? ") == "y":
+        more_input =True 
+    else:
+        more_input =False
 
-else:
-    print(result)
